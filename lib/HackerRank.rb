4876f08d4cc13aa735b433b1ecf48b72868dba60
Andy Wendt <andy@awendt.com>
2019-05-28 21:34:15 -0600

    Initial install

---
+++ lib/HackerRank.rb
@@ -0,0 +1,6 @@
+require "HackerRank/version"
+
+module HackerRank
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/HackerRank_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe HackerRank do
+  it "has a version number" do
+    expect(HackerRank::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
