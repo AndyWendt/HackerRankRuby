8d581bd4f12606d93e400625b4458fa8516da081
Andy Wendt <andy@awendt.com>
2019-05-28 22:26:36 -0600

    update scoring

--- lib/HackerRank.rb
+++ lib/HackerRank.rb
@@ -3,4 +3,9 @@ require "HackerRank/version"
 module HackerRank
   class Error < StandardError; end
   # Your code goes here...
+  def scoring(array)
+    array.each do |user|
+      user.update_score unless user.is_admin?
+    end
+  end
 end
