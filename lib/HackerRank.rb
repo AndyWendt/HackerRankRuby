3e911ff57f880222abf15eca56f7f59c903dfd27
Andy Wendt <andy@awendt.com>
2019-05-28 23:10:20 -0600

    Update when

--- lib/HackerRank.rb
+++ lib/HackerRank.rb
@@ -2,10 +2,34 @@ require "HackerRank/version"
 
 module HackerRank
   class Error < StandardError; end
-  # Your code goes here...
+
   def scoring(array)
     array.each do |user|
       user.update_score unless user.is_admin?
+    end
+  end
+
+  def self.identify_class(obj)
+    case obj
+    # if Hacker, output "It's a Hacker!"
+    when "Hacker"
+      puts "It's a Hacker!"
+
+    # if Submission, output "It's a Submission!"
+    when "Submission"
+      puts "It's a Submission!"
+
+    # if TestCase, output "It's a TestCase!"
+    when "TestCase"
+      puts "It's a TestCase!"
+
+    # if Contest, output "It's a Contest!"
+    when "Contest"
+      puts "It's a Contest!"
+
+    # for any other object, output "It's an unknown model"
+    else
+      puts "It's an unknown model"
     end
   end
 end
--- spec/HackerRank_spec.rb
+++ spec/HackerRank_spec.rb
@@ -6,4 +6,21 @@ RSpec.describe HackerRank do
   it "does something useful" do
     expect(true).to eq(true)
   end
+
+  it "correctly identifies classes" do
+    [
+        ["Hacker", "It's a Hacker!"],
+        ["Submission", "It's a Submission!"],
+        ["TestCase", "It's a TestCase!"],
+        ["Contest", "It's a Contest!"],
+        ["Foo", "It's an unknown model"],
+        ["Bar", "It's an unknown model"],
+    ].each do |input, expected|
+      save_stdout = $stdout
+      $stdout = StringIO.new
+      HackerRank.identify_class(input)
+      expect($stdout.string.strip).to eq(expected)
+      $stdout = save_stdout
+    end
+  end
 end
